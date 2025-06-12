d727ddf7b9afc4842c6bdbcbb0c662952d605794
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-12 05:49:13 +0000

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a specific string followed by a new line
+ *
+ * Description: Uses printf to display "with proper grammar, but the outcome is
+ *              a piece of art," followed by a newline.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
