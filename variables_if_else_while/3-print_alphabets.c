23b57080a9be498cf073128aefff9bb67f3db6b9
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-12 07:55:20 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,37 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: Your Name
+ * Desc: Prints the alphabet in lowercase then uppercase, followed by a new line.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter;
+
+    /* Print lowercase alphabet */
+    letter = 'a';
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    /* Print uppercase alphabet */
+    letter = 'A';
+    while (letter <= 'Z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
