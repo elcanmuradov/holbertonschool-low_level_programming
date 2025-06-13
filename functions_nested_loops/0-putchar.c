441aa60aa1959b2b271874b5585b207e1deb8d64
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-13 12:39:37 +0000

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+* File: 0-puthcar.c
+* Auth: Your Name
+* Desc: Prints all  alphabet charecters reversed , followed by a new line.
+*/
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("__putchar");
+putchar('\n');
+return (0);
+
+}
