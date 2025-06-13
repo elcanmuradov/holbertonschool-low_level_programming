a5596bc0205c68e046f64c8644d5d55662e5b159
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-13 11:30:28 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+/*
+* File: 3-print_alphabets.c
+* Auth: Your Name
+* Desc: Prints all  alphabet charecters, followed by a new line.
+*/
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+
+letter = 'a';
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+{
+    putchar(letter);
+    letter++;
+}
+}
+putchar('\n');
+return (0);
+
+}
