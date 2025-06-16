1c091872bfa674e781fb9c1531e6ebeef2d4e6c6
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 09:26:13 +0000

    4

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include "main.h"
 /*
  * reset_to_98 - the function to change value of n
- *  &n - value of the integer
- *
- * */
+ *  @n - value of the integer
+ * Return: void
+*/
 
 
 
