4de0d70c0b6dc6c391a19fc3228ad80eda57535e
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 10:07:48 +0000

    4

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ * _isupper - check the c and return a value
+ * @c - the value is checked
+ * Return - 0 or 1
+ *
+ *
+ *
+*/
+
+
+
+
+
+int _isupper(int c){
+if (c >= 'A' && c <= 'Z')
+{
+return 1;
+}
+else
+{
+return 0;
+}
+
+
+
+
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void jack_bauer(void);
+int print_last_digit(int);
+int add(int, int);
+void times_table(void);
+int _isupper(int c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void print_to_98(int n);
+
+
+
+#endif /* MAIN_H */
+
