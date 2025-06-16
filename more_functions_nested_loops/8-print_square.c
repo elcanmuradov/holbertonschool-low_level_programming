fbd311fac31b40c1a8622faff306f727c32a751f
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 11:41:26 +0000

    5

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,15 +9,15 @@
 
 void print_square(int size)
 {
-int row,col;
-if(size <= 0)
+int row, col;
+if (size <= 0)
 {
 _putchar('\n');
 return;
 }
-for(row = 0; row < size; row++)
+for (row = 0; row < size; row++)
 {
-for(col = 0; col < size; col++)
+for (col = 0; col < size; col++)
 {
 _putchar('#');
 }
