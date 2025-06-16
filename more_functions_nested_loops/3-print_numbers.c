6954b5ae9dccee71a3962855d23409d16b50dacd
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 10:26:43 +0000

    5

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Checks for uppercase character
+ * @a: The character to check
+ * @b: The number two
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+
+void print_numbers(void)
+{
+for (int i; i <= 9; i++)
+{
+_putchar('0' + i);
+}
+}
