eff3d3376e40b47ef7a809c1dbdd28af722b3652
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 11:31:08 +0000

    5

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include"main.h"
+void print_diagonal(int n)
+{
+int i;
+if(n<=0)
+{
+_putchar('\n');
+return;
+}
+for(i=0;i<n;i++)
+{
+int j;
+for(j=0;j<i;j++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+
