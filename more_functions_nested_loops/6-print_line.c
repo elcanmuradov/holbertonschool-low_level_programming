d0faec6cbc5df622b557eda3129845cf2699d9c2
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-16 11:13:18 +0000

    5

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a new line
+ * Return:void
+ * @n: number
+ *
+*/
+
+void print_line(int n)
+{
+for (n; n > 0; n--)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
