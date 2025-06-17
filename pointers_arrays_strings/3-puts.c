86601c39d6861ea19b545549a09984bd337f42f2
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-17 14:55:20 +0000

    4

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints line
+ * @str: a string
+ *
+*/
+
+void _puts(char *str)
+{
+int count = 0;
+
+while (str[count] != '\0')
+{
+_putchar('%c', str[count];
+count++;
+}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ void print_to_98(int n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
+void _puts(char *str);
 
 
 
