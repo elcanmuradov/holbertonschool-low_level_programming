ffa11cd77944a2dc3f879b5158a9ad46664dd073
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-18 12:03:01 +0000

    4

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_array - function
+ * @a: Array
+ * @n: length of Array
+*/
+
+void print_array(int *a, int n)
+{
+while (n - 1  >=  0)
+{
+printf("%d, ", a[n]);
+n--;
+}
+}
