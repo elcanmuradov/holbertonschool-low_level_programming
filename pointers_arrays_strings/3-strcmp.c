d12566fb8a701efcabaa700764b41188800d163e
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-19 15:16:25 +0000

    4

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - sdofsf
+ * @s1: ofkwoef
+ * @s2: sdfsf
+ * Return: wfwf
+*/
+
+
+int _strcmp(char *s1, char *s2)
+{
+
+int i = 0;
+int r = 0;
+
+while (s1[i] != '\0')
+{
+r = r + s1[i] - s2[i];
+i++;
+}
+return (r);
+}
+}
