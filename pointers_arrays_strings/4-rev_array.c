15418a9a0590ca5199d6bda83400e656363745fc
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-20 10:47:50 +0000

    4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - function
+ * @a: array
+ * @n: length of array
+ *
+*/
+void reverse_array(int *a, int n)
+{
+int b[];
+int i = 0;
+while (n > 0)
+{
+b[i] = a[n - 1];
+i++;
+n--;
+}
+a[] = b[];
+}
