6465a1afbb06a28bb7c861b16650428f1035bf71
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-20 11:19:48 +0000

    4

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - function
+ * @s: string
+ * Return: string
+*/
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+i++;
+}
+return (s);
+}
