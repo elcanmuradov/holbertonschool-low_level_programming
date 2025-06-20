4f36736b920d8db2fdb379506f647bae313d6747
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-20 10:11:01 +0000

    4

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 
 int i = 0;
-int r = 0;
+int r;
 
-while (s1[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0' &&  s1[i] == s2[i])
 {
-r = r + s1[i] - s2[i];
 i++;
 }
+r = s1[i] - s2[i];
 return (r);
 }
 
