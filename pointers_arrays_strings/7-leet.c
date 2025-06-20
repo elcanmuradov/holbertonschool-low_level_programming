d6a5713608aa1addb17ad54d6b177101bd3c754b
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-20 11:47:59 +0000

    4

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,20 +2,26 @@
 /**
  * leet - function
  * @s: string
+ * Return: String
 */
 char *leet(char *s)
 {
-int i = 0 ;
+int i = 0;
+int k = 0;
+
 char a[15] = "a4e3o0t7l1A4E3O0T7L1";
 while (s[i] != '\0')
 {
-for (int k = 0; k < 15;k++)
+while (k < 15)
 {
-if(s[i] == a[k])
+if (s[i] == a[k])
 {
 s[i] = a[k + 1];
 }
+k++
 }
 i++;
+k = 0;
 }
+return (s);
 }
