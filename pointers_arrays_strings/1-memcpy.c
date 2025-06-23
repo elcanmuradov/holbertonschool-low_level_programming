8a1d417009b58a12de29ed6ee6c5816a383872ed
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-23 09:48:14 +0000

    3

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int count = 0;
 
-while (count < n)
+while (count <= n)
 {
 dest[count] = *src;
 count++;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,7 +32,7 @@ char *cap_string(char *);
 void print_array(int *a, int n);
 char *leet(char *s);
 char *_memset(char *s, char b, unsigned int n);
-
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 
