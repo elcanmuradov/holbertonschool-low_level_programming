132527034e3781d3dde37f5970dc707dff3c281f
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-24 13:40:05 +0000

    4

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - function
+ * @accept: arg1
+ * @s: arg2
+ * Return: count
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0;
+int c = 0;
+int k = 0;
+while (s[c] != '\0')
+{
+while (accept[k] != '\0')
+{
+if (accept[k] == s[c])
+{
+i++;
+k = 0;
+break;
+}
+}
+c++;
+}
+return (i)
+}
