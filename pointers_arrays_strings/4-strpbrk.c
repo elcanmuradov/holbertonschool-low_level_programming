97d106e02bd55b9604d49b0ac1977dfe83e82f7a
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-24 13:48:52 +0000

    4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - function
+ * @accept: arg1
+ * @s: arg2
+ * Return: count
+*/
+char *_strpbrk(char *s, char *accept);
+{
+int c = 0;
+int k = 0;
+while (s[c] != '\0')
+{
+while (accept[k] != '\0')
+{
+if (accept[k] == s[c])
+{
+return (accept[k]);
+}
+k++;
+}
+c++;
+}
+}
