c5d008d7d9717ec2452beacdf4961cbe221d425b
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-25 09:18:15 +0000

    4

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function
+ * @s: array
+*/
+
+void _puts_recursion(char *s)
+{
+_putchar(*s);
+if (*(s + 1) != '\0')
+{
+_puts_recursion(*(s + 1));
+}
+}
