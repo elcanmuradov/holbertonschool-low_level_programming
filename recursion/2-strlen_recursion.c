b387a5b919575dd61e8d2fd09857c0226d4dd7ed
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-25 10:05:36 +0000

    4

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function
+ * @s: array
+ * Return: length of s
+*/
+
+int _strlen_recursion(char *s)
+{
+int n = 0;
+if (*s != '\0')
+{
+i++;
+_strlen_recursion(s+1)
+}
+else
+{
+return i;
+}
+}
