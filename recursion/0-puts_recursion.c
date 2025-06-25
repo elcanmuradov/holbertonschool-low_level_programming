90c1378b78c9570f5e0a52d27a29eeefe1e33337
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-25 09:25:16 +0000

    4

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,10 +7,13 @@
 
 void _puts_recursion(char *s)
 {
-_putchar(*s);
-if (*(s + 1) != '\0')
+if (*(s + 1) != '\0' && *s != '\0')
 {
 _puts_recursion((s + 1));
+}
+else if (*s != '\0')
+{
+_putchar('\0');
 }
 _putchar('\n');
 }
