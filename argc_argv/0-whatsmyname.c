3df3c65c12f1fa6fb6b3b7003c724a05f0e39b99
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-26 14:28:23 +0000

    4

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - function
- * @argc: arg1
- * @argv: arg2
- * Return: 0
-*/
-int main(int argc, char *argv[])
+ * main - Prints the program's name, including the path.
+ * @argc: Number of command-line arguments (unused).
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
