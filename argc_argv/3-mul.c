9d643dd8d2826da94161ce9d16d520ba6c428c4b
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-26 14:46:36 +0000

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,15 +6,18 @@
  * @argc: arg2
  * Return: 0
 */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 if (argc - 1 == 2)
 {
-printf("%d\n", argv[1] * argv[2]);
+int mul = argv[1] * argv[2];
+
+printf("%d\n", mul);
+return (0);
 }
 else
 {
 printf("Error\n");
+return (1);
 }
-return (0);
 }
