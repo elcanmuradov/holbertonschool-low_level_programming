f7287c53ad03b27cdfb17ddc7effc4ee989622d1
elcanmuradov <elcanmuradov25@gmail.com>
2025-06-26 14:32:29 +0000

    4

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0
 */
 
-int main(int argc,char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-printf("%d\n", argc);
+printf("%d\n", argc -1);
 return (0);
 }
