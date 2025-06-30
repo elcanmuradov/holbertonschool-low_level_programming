763076e62e5a92e1593bdf72b807c9223734bfc2
elcanmuradov <elcanmuradov64@gmail.com>
2025-06-30 12:36:23 +0000

    4

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ * @width: arg1
+ * @height: arg2
+ * Return: grid
+*/
+int **alloc_grid(int width, int height)
+{
+int w = 0, h = 0, *grid;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+grid = malloc(width * height * sizeof(int));
+while (h < height)
+{
+while (w < width)
+{
+grid[h][w] = 0;
+}
+h++;
+w = 0;
+}
+return (grid);
+}
