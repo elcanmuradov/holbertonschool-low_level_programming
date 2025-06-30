2c8caa20e08e7f05ee95b8928dec6bdd84b7bd68
elcanmuradov <elcanmuradov64@gmail.com>
2025-06-30 10:27:38 +0000

    4

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,20 +10,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-if (size != 0)
-{
-unsigned int i = 0;
-char *arr;
-arr = malloc(size * sizeof(char));
-while (i < size)
+if (size == 0)
+return (NULL);
+char *arr = malloc(size * sizeof(char));
+if (arr == NULL)
+return (NULL);
+for (unsigned int i = 0; i < size; i++)
 {
 arr[i] = c;
-i++;
 }
 return (arr);
-}
-else
-{
-return (NULL);
-}
 }
