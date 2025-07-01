f214946a844394cf63e1357776e916ab2d86c171
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-01 15:52:48 +0000

    4

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - funct
+ * @s1: arg1
+ * @s2: arg2
+ * @n: arg3
+ * Return: char
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int ss1 = 0, ss2 = 0, c = 0;
+char *arr;
+while (s1[ss1] != '\0')
+{
+ss1++;
+}
+while (s2[ss2] != '\0')
+{
+ss2++;
+}
+if (ss2 <= n)
+{
+n = ss2;
+}
+arr = malloc(sizeof(char) * ss1 + (sizeof(char) * n));
+if (arr == NULL)
+{
+return (NULL);
+}
+while (c < ss1 + n)
+{
+if (c < ss1)
+{
+arr[c] = s1[c];
+}
+else
+{
+arr[c] = s2[c - n];
+}
+}
+return (arr);
+}
