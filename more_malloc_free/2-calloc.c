4e4c4c9e67c41a4e282b6f59a058e8da3a419682
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-02 11:16:55 +0000

    4

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,10 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _calloc - funct
+ * @nmemb: arg1
+ * @size: arg2
+ * Return: void
+*/
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
+int i = 0;
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
@@ -13,6 +19,11 @@ ptr = malloc(nmemb * size);
 if (ptr == NULL)
 {
 return (NULL);
+}
+while (i < nmemb)
+{
+ptr[i] = 0;
+i++;
 }
 return (ptr);
 }
