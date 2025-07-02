3a10f691690ac49935ee6f885f76cc842c2f9eb3
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-02 11:13:12 +0000

    4

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -5,11 +5,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
-if (nmeb == 0 || size == 0)
+if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-ptr = malloc(nmeb * size);
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
 {
 return (NULL);
