b34c4624be4b4c0210387560186bf8d981e364c6
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-03 14:29:38 +0000

    4

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "dog.h"
 #include <stdlib.h>
 
 /**
@@ -11,7 +10,7 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d.name = name;
-d.age = age;
-d.owner = owner;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
