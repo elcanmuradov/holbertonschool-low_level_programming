181b9f4eca501ec223e84efbeee0eec83711fcc5
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-04 11:43:06 +0000

    3

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,8 +12,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 if (name != NULL && age != NULL && owner != NULL)
 {
+int i = 0;
+int k = 0;
+while(name[i] != '\0')
+{
+i++;
+}
+while(owner[k] != '\0')
+{
+k++;
+}
+dog_t->name = malloc(i * sizeof(char));
+if (dog_t->name == NULL)
+{
+return (NULL);
+}
 dog_t->name = name;
+
 dog_t->age = age;
+dog_t->owner = malloc(k * sizeof(char));
+if (dog_t->owner == NULL)
+{
+return (NULL);
+}
 dog_t->owner = owner;
 return (dog_t);
 }
