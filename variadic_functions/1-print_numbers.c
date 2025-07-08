4262afbf9d3c133a56f3065f49795b37ebcd0cf5
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-08 08:41:23 +0000

    4

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,7 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i = 0;
 va_list args;
 va_start(args, n);
-
+if (separator == NULL)
+{
+separator = ""
+}
 while (i < n)
 {
 if (i != n - 1)
