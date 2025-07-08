452cebc75486a494894072f39a4d8890a9e35d29
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-08 08:12:48 +0000

    4

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdarg.h>
+
+
+/**
+ * sum_them_all - funct
+ * @n: arg
+ * Return: count
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+int c = 0;
+unsigned int i = 0;
+va_list args;
+
+va_start(args, n);
+while (i < n)
+{
+c += va_arg(args, int);
+i++;
+}
+va_end(args);
+return (c);
+}
