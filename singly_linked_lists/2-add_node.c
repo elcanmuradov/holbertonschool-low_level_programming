f899319c98bcfdf843a2a1cc78ce80c0dec3f640
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-22 07:13:14 +0000

    4

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
+#define _GNU_SOURCE
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * add_node - Adds a new node at the beginning of a list_t list
  * @head: Double pointer to the head of the list
