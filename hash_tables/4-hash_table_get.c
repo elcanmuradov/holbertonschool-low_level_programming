ebac7c930a1e7967208d2b75788f15a184cc2664
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-27 18:35:46 +0000

    3

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -6,7 +6,7 @@
  * @ht: the hash table you want to look into
  * @key: the key you are looking for
  *
- * Return: the value associated with the element, or NULL if key couldn't be found
+ * Return: the valuent, or NULL if key couldn't be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
