3fa8a577684306aa9538c67d77798147b4d9be8a
elcanmuradov <elcanmuradov25@gmail.com>
2025-07-27 18:29:24 +0000

    4

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,11 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - funct
+ * @str: arg1
+ * Return: hash
+*/
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
@@ -6,7 +14,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) + c;
+hash = ((hash << 5) + hash)+c;
 }
 return (hash);
 }
